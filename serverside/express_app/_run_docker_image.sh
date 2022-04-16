33d3e6bff0e3ba5f16d1819317656d829b024ea0
Thomas Serrano <serrat@uw.edu>
2022-04-15 17:42:17 -0700

    change back to 3308 bc that might be more work

--- serverside/express_app/_run_docker_image.sh
+++ serverside/express_app/_run_docker_image.sh
@@ -4,7 +4,7 @@ docker stop ftv-backend-1.0
 docker rm ftv-backend-1.0
 
 docker run -d \
--p 4000:3000 \
+-p 3308:3000 \
 --network todo-app --network-alias express \
 --name ftv-backend-1.0 \
 serrat/ftv-backend-1.0
