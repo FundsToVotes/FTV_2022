551a47f50b4976d944753c11aea57e389cac683a
haileymeister <49212221+haileymeister@users.noreply.github.com>
2022-04-22 13:29:42 -0700

    mobile

--- clientside/src/Components/Top10Pie.js
+++ clientside/src/Components/Top10Pie.js
@@ -1,51 +1,25 @@
-
-import Plot from  "react-plotly.js";
+import Plot from "react-plotly.js";
 import React from "react";
 
 function Top10Pie(props) {
   const { repsData } = props;
-  console.log(repsData)
+  console.log(repsData);
   let returned;
-  
+
   if (repsData.name) {
     //somehow get rep name
-    let top10 = repsData.data
+    let top10 = repsData.data;
     console.log(top10);
 
     //fetch mans, do things with data
     let values = [];
     let labels = [];
-    
+
     top10.forEach((d) => {
       values.push(d.total);
       labels.push(d.industry_name);
     });
     var colorScheme = [];
-    // var colorBlind = [
-    //   "#003f5c",
-    //   "#2f4b7c",
-    //   "#665191",
-    //   "#a05195",
-    //   "#d45087",
-    //   "#f95d6a",
-    //   "#ff7c43",
-    //   "#ffa600",
-    //   "#ffaa68",
-    //   "#fff80e",
-    // ];
-
-    // var purpleOrange = [
-    //   "#291725",
-    //   "#5c0049",
-    //   "#7a134f",
-    //   "#972953",
-    //   "#b13f56",
-    //   "#c95859",
-    //   "#de725c",
-    //   "#f08d61",
-    //   "#ffaa68",
-    //   "#fcc89f",
-    // ];
 
     var redWhiteGreen = [
       "#1a4242",
@@ -90,18 +64,11 @@ function Top10Pie(props) {
       width: "600",
     };
 
-    returned = <Plot
-      data={data}
-      layout={layout}
-      />
+    returned = <Plot data={data} layout={layout} />;
   }
-  
+
   // Plotly.newPlot("pie" + data.cid, data, layout);
 
-  return (
-    <div>
-      {returned}
-    </div>
-  );
+  return <div>{returned}</div>;
 }
 export default Top10Pie;
