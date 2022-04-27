5f2b34cee6990011170f1898f3bcc08163a41c7d
Thomas Serrano <serrat@uw.edu>
2022-04-26 18:55:47 -0700

    make sure it is the right connection in app.js

--- serverside/express_app/app.js
+++ serverside/express_app/app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.options("*", cors())
 
-// // for building and deploying. 
+// for building and deploying. 
 // var con = mysql.createPool({
 //     connectionLimit: 5,
 //     host:"mysql",
